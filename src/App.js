ca4f59927567f573eef03f90750938174c872d69
Uriel Meneses <saidmeneses1@live.com>
2020-02-04 20:51:47 -0600

    first commit, data is coming in

--- src/App.js
+++ src/App.js
@@ -1,26 +1,44 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
+import axios from 'axios'
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+
+  constructor(){
+    super();
+
+    this.state ={
+      weatherData: []
+    }
+  }
+
+  componentDidMount(){
+    this.fetchingWeater()
+  }
+
+  fetchingWeater = () => {
+    const params = {
+      access_key: '216b83b59e86240052cbff9a8c4688dd',
+      query: 'Kaufman TX',
+      units: 'f'
+    }
+    axios
+    .get('http://api.weatherstack.com/current?access_key=216b83b59e86240052cbff9a8c4688dd&query=Kaufman%TX&units=f')
+    .then(res => {
+      // console.log(response.data)
+      this.setState({weatherData: res.data})
+    }).catch(error => {
+      console.log(error)
+    })
+  }
+  
+  render(){
+    return(
+      <div>
+        Hello
+      </div>
+    )
+  }
 }
 
 export default App;
