5c8a0ffa73819ab37e29a676b25011b9fdec61ad
Uriel Meneses <saidmeneses1@live.com>
2020-03-05 22:04:00 -0600

    structured data to come in

--- src/App.js
+++ src/App.js
@@ -8,7 +8,8 @@ class App extends Component {
     super();
 
     this.state ={
-      weatherData: []
+      weatherData: [],
+      weatherData2: [],
     }
   }
 
@@ -17,25 +18,57 @@ class App extends Component {
   }
 
   fetchingWeater = () => {
-    const params = {
-      access_key: '216b83b59e86240052cbff9a8c4688dd',
-      query: 'Kaufman TX',
-      units: 'f'
-    }
     axios
     .get('http://api.weatherstack.com/current?access_key=216b83b59e86240052cbff9a8c4688dd&query=Kaufman%TX&units=f')
     .then(res => {
-      // console.log(response.data)
-      this.setState({weatherData: res.data})
+      // console.log(res.data)
+      this.setState({
+        weatherData: [res.data.current],
+        weatherData2: [res.data.location]
+      })
     }).catch(error => {
       console.log(error)
     })
   }
-  
+
+
   render(){
+  let displayData = this.state.weatherData.map((el, id) => {
+      console.log(el.temperature)
+      if(!this.state.weatherData){
+        return (
+          <div>
+            <p>Loading data...</p>
+          </div>
+        )
+      } else {
+        return (
+          <div key={id}>
+            <ul>
+              <li>{el.temperature} F</li>
+              <li>{el.feelslike}</li>
+              <li>{el.humidity} %</li>
+              <li>{el.wind_speed} mph</li>
+              <li>{el.humidity}</li>
+            </ul>
+          </div>
+        )
+      }
+    })
+
+    let displayData2 = this.state.weatherData2.map((el,id) => {
+      return (
+        <div key={id}>
+          <h1>{el.name}, {el.region}</h1>
+        </div>
+      )
+    })
+
     return(
       <div>
-        Hello
+        {displayData2}
+        {displayData}
+
       </div>
     )
   }
